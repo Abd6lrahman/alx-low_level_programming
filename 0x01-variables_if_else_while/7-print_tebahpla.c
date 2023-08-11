bf07929e91178475e2e7bd2ddbccfebb00c39ef0
Abdelrahman Moustafa <015abdo150@gmail.com>
2023-08-12 02:47:01 +0300

    my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:
+ * This program prints the lowercase alphabet in reverse,
+ * followed by a new line, using only the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter = 'z';
+
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+
+putchar('\n');
+
+return (0);
+}
+
