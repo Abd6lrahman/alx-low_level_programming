3593954a72a16a4ab90167b19661fc53ce56f39c
Abdelrahman Moustafa <015abdo150@gmail.com>
2023-08-18 05:00:06 +0300

    my commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,36 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 /**
- * _isupper - Check if a character is uppercase.
- * @c: The character to check.
- *
- * Return: 1 if the character is uppercase, 0 otherwise.
- */
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
 int _isupper(int c);
-
-/**
- * _isdigit - Check if a character is a digit (0-9).
- * @c: The character to check.
- *
- * Return: 1 if the character is a digit, 0 otherwise.
- */
 int _isdigit(int c);
-
-/**
- * mul - Multiply two integers.
- * @a: The first integer.
- * @b: The second integer.
- *
- * Return: The product of a and b.
- */
 int mul(int a, int b);
-
-/**
- * print_numbers - Print numbers from 0 to 9.
- */
 void print_numbers(void);
-void print_most_numbers(void);
-
-#endif /* MAIN_H */
-
+void print_most_numbers(void);)
