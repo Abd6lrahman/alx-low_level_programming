935a70e55eb3aad1e355472737307a6e1a898606
Abdelrahman Moustafa <015abdo150@gmail.com>
2023-08-19 04:10:05 +0300

    my commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
-*main - header file for protoypes
-*void protoypes (void)
-*int protoypes (int)
-*/
+
+#include <stdio.h>
+
+int _putchar(char c);
 int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);)
+
+#endif /* MAIN_H */
